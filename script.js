0e15c395fa69310bedbb7c458ccddec315667cdd
yna-pro <mkrowj@proton.me>
2026-03-08 10:41:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.addEventListener("scroll", function() {
 
+  const arrow = document.querySelector(".scroll-down");
+
+  if (window.scrollY > 50) {
+    arrow.classList.add("hide");
+  } else {
+    arrow.classList.remove("hide");
+  }
+
+});
